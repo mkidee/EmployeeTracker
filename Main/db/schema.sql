66831584a023be8da7856430ed7d2e97f2b2ebf6
mkidee <mckaymemmott@gmail.com>
2023-05-14 23:25:40 -0600

    created schema.sql for skeleton of database

--- Main/db/schema.sql
+++ Main/db/schema.sql
@@ -1,2 +1,42 @@
-DROP DATABASE IF EXISTS empTrack;
-CREATE DATABASE IF EXISTS empTrack;
+DROP DATABASE IF EXISTS emp_db;
+CREATE DATABASE emp_db;
+
+USE emp_db;
+
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    depName VARCHAR(30) NOT NULL,
+);
+
+CREATE TABLE employee_role (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT,
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+    ON DELETE SET NULL
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT REFERENCES employee(id),
+    FOREIGN KEY (role_id)
+    REFERENCES employee_role(id)
+    ON DELETE SET NULL
+);
+
+-- SHOW TABLES;
+
+-- SELECT "department" AS "Table";
+-- DESCRIBE department;
+
+-- SELECT "employee_role" AS "Table";
+-- DESCRIBE employee_role;
+
+-- SELECT "employee" AS "Table";
+-- DESCRIBE employee;
+

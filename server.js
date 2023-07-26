e584958f593be53ed38f3f820b6ee3aa119a1487
mkidee <mckaymemmott@gmail.com>
2023-07-26 10:07:18 -0600

    answers for questions

--- server.js
+++ server.js
@@ -43,3 +43,53 @@ function questions() {
         }
 
     ])
+
+        .then((answers) => {
+            console.log(answers);
+            let choices = answers.choices;
+            switch (choices) {
+                case 'viewAllDepartments':
+                    db.viewAllDepartments().then((res) => {
+                        console.table(res[0])
+                        questions();
+                    })
+
+                    break;
+                case 'viewAllRoles':
+                    db.viewAllRoles().then((res) => {
+                        console.table(res[0]);
+                        questions();
+                    });
+
+                    break;
+                case 'viewAllEmployees':
+                    db.viewAllEmployees().then((res) => {
+                        console.table(res[0]);
+                        questions();
+                    });
+                    break;
+                // if add department, call the add department meothd and prompt the user for the department name
+                case 'addDepartment':
+                    addDepartment();
+                    break;
+
+                // if choice is add employee, call the add employee method
+                case 'addRole':
+                    addRole();
+                    break;
+
+                case 'addEmployee':
+                    addEmployee();
+                    break;
+
+                case 'updateEmployeeRole':
+                    updateEmployeeRole();
+                    break;
+                // if choice is exit then exit the application
+                case 'exit':
+                    console.log('Bye.');
+                    connection.end();
+                    break;
+        }
+    })
+};

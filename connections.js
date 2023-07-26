d5f5a111ee14ff1c62767e90918a3d82ae4f5192
mkidee <mckaymemmott@gmail.com>
2023-07-26 09:59:01 -0600

    forgot to include connections in last update

--- connections.js
+++ connections.js
@@ -1,25 +1,12 @@
-//mysql import
-const mysql = require('mysql2');
+const mysql2 = require('mysql2');
 
-//load in the env file
-require('dotenv').config();
+const MySqlconnection = mysql2.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'emp_DB'
+    },
+);
 
-//using env connections
-const connect = mysql.createConnection({
-    host: 'localhost',
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: 'emp_db'
-    });
-
-    connection.connect((err) => {
-        if (err) {
-            console.error('Error connecting to database: ', err);
-            return;
-        }
-    console.log('\nConnected to employee tracking database.');
-
-});
-
-//export the connect const
-module.exports = connect;
+module.exports = MySqlconnection;
